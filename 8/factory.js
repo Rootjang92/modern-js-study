11bdba8967a851ba4e4e015ce66a6dcb551bd789
Rootjang92 <janggeunho7@gmail.com>
2019-01-02 14:15:48 +0900

    closure factory

---
+++ 8/factory.js
@@ -0,0 +1,12 @@
+function makeMultiplier(x) {
+  return function(y) {
+    return x*y;
+  };
+}
+
+var multi2 = makeMultiplier(2);
+var multi10 = makeMultiplier(10);
+console.log(multi2(3)); // 6
+console.log(multi10(3)); // 30
+
+// 값을 x배하는 함수 팩토리
